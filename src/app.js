d9300c4a0ad32c220e330196be149b5de5fe4c57
Jay <tidzmtm12@gmail.com>
2022-01-09 15:42:25 +0900

    createElement

--- src/app.js
+++ src/app.js
@@ -1,36 +1,18 @@
-import { createDOM, render } from "./react";
+import { createDOM, createElement, render } from "./react";
 
-const vdom = {
-  tag: "p",
-  props: {},
-  children: [
-    {
-      tag: "h1",
-      props: {},
-      children: ["React 만들기"],
-    },
-    {
-      tag: "ul",
-      props: {},
-      children: [
-        {
-          tag: "li",
-          props: { style: "color: blue" },
-          children: ["첫 번째 아이템"],
-        },
-        {
-          tag: "li",
-          props: { style: "color: green" },
-          children: ["두 번째 아이템"],
-        },
-        {
-          tag: "li",
-          props: { style: "color: red" },
-          children: ["세 번째 아이템"],
-        },
-      ],
-    },
-  ],
-};
+const vdom = createElement(
+  "p",
+  {},
+  createElement("h1", {}, "React 만들기"),
+  createElement(
+    "ul",
+    {},
+    createElement("li", { style: "color: blue" }, "첫 번째 아이템"),
+    createElement("li", { style: "color: green" }, "두 번째 아이템"),
+    createElement("li", { style: "color: red" }, "세 번째 아이템")
+  )
+);
+
+console.log(vdom);
 
 render(vdom, document.querySelector("#root"));
