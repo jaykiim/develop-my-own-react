6158353cd1d40392f2662262bd83149ce7f7b0a0
Jay <tidzmtm12@gmail.com>
2022-01-09 17:08:44 +0900

    @jsx

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
+/* @jsx createElement */
+
 import { createDOM, createElement, render } from "./react";
 
-const vdom = createElement(
-  "p",
-  {},
-  createElement("h1", {}, "React 만들기"),
-  createElement(
-    "ul",
-    {},
-    createElement("li", { style: "color: blue" }, "첫 번째 아이템"),
-    createElement("li", { style: "color: green" }, "두 번째 아이템"),
-    createElement("li", { style: "color: red" }, "세 번째 아이템")
-  )
+const vdom = (
+  <p>
+    <h1>React 만들기</h1>
+    <ul>
+      <li style="color: blue">첫 번째 아이템</li>
+      <li style="color: green">두 번째 아이템</li>
+      <li style="color: red">세 번째 아이템</li>
+    </ul>
+  </p>
 );
-
-console.log(vdom);
 
 render(vdom, document.querySelector("#root"));
